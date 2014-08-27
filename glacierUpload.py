4674fab7c03354488ec9a59cf3c5946cb312c260
Sam Caldwell <sam@edgecase.io>
2014-08-27 11:32:59 -0500

    Added license to files.  Fixed dependency issue.  Fixed misc bug.

--- glacierUpload.py
+++ glacierUpload.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 #
 # glacerUpload.py
-# (c) 2014 Compare Metrics, Inc.
-#     Sam Caldwell (sam@edgecase.io)
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Edge Case, Inc.
+# Author: Sam Caldwell (sam@edgecase.io)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to 
+# deal in the Software without restriction, including without limitation the 
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS # IN THE SOFTWARE.
 #
 # This is a quick and dirty script to upload files to glacier from 
 # the local file system.  Use --help or -h for usage informaiton.
@@ -12,7 +32,7 @@ import sys,traceback
 import os
 import boto 
 import argparse
-import datetime
+import time
 
 def showException(msg,err):
     print(msg +":"+ str(err))
@@ -63,7 +83,7 @@ def upload(awsKeyId,awsSecret,awsVault,file,inventory,description=None):
             showException("ERROR_CREATING_VAULT:",err)
     startTime=time.time()
     try:
-        print("\n\nStarting upload [{}] at {}\n\n".format(fileName,startTime))
+        print("\n\nStarting upload [{}] at {}\n\n".format(file,startTime))
         archive_id = vault.upload_archive(file)
     except Exception as err:
         showException("ERROR_UPLOADING",err)
