4de2c9f15176aeb10753e0cf277a9a472af1f8f9
Sam Caldwell <sam@edgecase.io>
2014-08-26 20:12:25 -0500

    tab->space fix.

--- glacierUpload.py
+++ glacierUpload.py
@@ -15,79 +15,79 @@ import argparse
 import datetime
 
 def showException(msg,err):
-	print msg +":"+ str(err)
-	print("-"*60)
-	traceback.print_exc(file=sys.stdout)
-	print("-"*60)
-	print("")
-	sys.exit(1)
+    print msg +":"+ str(err)
+    print("-"*60)
+    traceback.print_exc(file=sys.stdout)
+    print("-"*60)
+    print("")
+    sys.exit(1)
 
 def upload(awsKeyId,awsSecret,awsVault,fileName,description):
 
-	connection=None
-	vault=None
-	
-	if not os.path.isfile(fileName) :
-	    print("File not found: {}".format(fileName))
-	    sys.exit(1);
-	try:
-		connection = boto.connect_glacier(
-									aws_access_key_id=awsKeyId,
-                                	aws_secret_access_key=awsSecret
+    connection=None
+    vault=None
+    
+    if not os.path.isfile(fileName) :
+        print("File not found: {}".format(fileName))
+        sys.exit(1);
+    try:
+        connection = boto.connect_glacier(
+                                    aws_access_key_id=awsKeyId,
+                                    aws_secret_access_key=awsSecret
         )
-	except Exception as err:
-		showException("ERROR_CONNECTING",err)
+    except Exception as err:
+        showException("ERROR_CONNECTING",err)
 
-	try:
-		vault = layer2.get_vault(awsVault)
-	except Exception as err:
-		try:
-			print("ERROR: Failed to get vault.  trying to create [{}]".format(
-																		awsVault
-																	)
-			)
-			vault = connection.create_vault(awsVault)
-		except Exception as err:
-			showException("ERROR_CREATING_VAULT:",err)
+    try:
+        vault = layer2.get_vault(awsVault)
+    except Exception as err:
+        try:
+            print("ERROR: Failed to get vault.  trying to create [{}]".format(
+                                                                        awsVault
+                                                                    )
+            )
+            vault = connection.create_vault(awsVault)
+        except Exception as err:
+            showException("ERROR_CREATING_VAULT:",err)
 
-	try:
-		print("\n\nStarting upload [{}] at {}\n\n".format(
-														fileName,
-														datetime.datetime.now())
-		)
-		archive_id = vault.upload_archive(fileName)
-	except Exception as err:
-		showException("ERROR_UPLOADING",err)
+    try:
+        print("\n\nStarting upload [{}] at {}\n\n".format(
+                                                        fileName,
+                                                        datetime.datetime.now())
+        )
+        archive_id = vault.upload_archive(fileName)
+    except Exception as err:
+        showException("ERROR_UPLOADING",err)
 
-	print("Done id:{} time:{}".format(archive_id,datetime.datetime.now()))
+    print("Done id:{} time:{}".format(archive_id,datetime.datetime.now()))
 
 parser=argparse.ArgumentParser(description="uploader for Amazon AWS glacier")
 
 parser.add_argument("--glacierVault",
-	help="AWS Glacier Vault URI",
-	dest="vault",
-	type=str
+    help="AWS Glacier Vault URI",
+    dest="vault",
+    type=str
 )
 parser.add_argument("--awsKeyId",
-	help="AWS API Key Id (aws_awsKeyId)",
-	dest="keyId",
-	type=str
+    help="AWS API Key Id (aws_awsKeyId)",
+    dest="keyId",
+    type=str
 )
 parser.add_argument("--secret",
-	help="AWS secret (aws_secret_access_key)",
-	dest="secret",
-	type=str
+    help="AWS secret (aws_secret_access_key)",
+    dest="secret",
+    type=str
 )
 parser.add_argument("--sourceFile",
-	help="Source file to be uploaded.",
-	dest="sourceFile",
-	type=str
+    help="Source file to be uploaded.",
+    dest="sourceFile",
+    type=str
 )
 parser.add_argument("--description",
-	help="optional description.",
-	dest="desc",
-	default="",
-	type=str
+    help="optional description.",
+    dest="desc",
+    default="",
+    type=str
 )
 args=parser.parse_args()
 upload( args.keyId, args.secret, args.vault, args.sourceFile, args.desc )
